d7c3ccce1f6369add4159f7dff47710d7a6b99ec
zkboys <876213774@qq.com>
2021-08-24 16:52:06 +0800

    :art: 整理代码

--- generator/gen/routes.js
+++ generator/gen/routes.js
@@ -11,102 +11,85 @@ const router = express.Router();
 
 router.get('/tables', async (req, res) => {
     const {dbUrl} = req.query;
-    try {
-        const tables = await getTableNames(dbUrl);
+    const tables = await getTableNames(dbUrl);
 
-        if (tables && tables.length) {
-            for (const table of tables) {
-                table.columns = await getTableColumns(dbUrl, table.name);
-            }
+    if (tables && tables.length) {
+        for (const table of tables) {
+            table.columns = await getTableColumns(dbUrl, table.name);
         }
-
-        res.send({tables, ignoreFields: COMMON_EXCLUDE_FIELDS});
-    } catch (e) {
-        res.status(400).send(e);
     }
+
+    res.send({tables, ignoreFields: COMMON_EXCLUDE_FIELDS});
 });
 
 router.post('/preview', async (req, res) => {
-    try {
-        const {tables} = req.body;
+    const {tables} = req.body;
 
-        let configs = [];
-        tables.map(getConfigFromDbTable).forEach(item => {
-            configs = configs.concat(item);
+    let configs = [];
+    tables.map(getConfigFromDbTable).forEach(item => {
+        configs = configs.concat(item);
+    });
+
+    const result = [];
+    configs.forEach(cfg => {
+        let {template} = cfg;
+        result.push({
+            config: cfg,
+            code: require(template)(cfg),
         });
+    });
 
-        const result = [];
-        configs.forEach(cfg => {
-            let {template} = cfg;
-            result.push({
-                config: cfg,
-                code: require(template)(cfg),
-            });
-        });
-
-        res.send(result);
-    } catch (e) {
-        console.log(e);
-        res.status(400).send(e);
-    }
+    res.send(result);
 });
 
 router.post('/tables', async (req, res) => {
-    try {
-        const {tables} = req.body;
+    const {tables} = req.body;
 
-        let configs = [];
-        tables.map(getConfigFromDbTable).forEach(item => {
-            configs = configs.concat(item);
-        });
+    let configs = [];
+    tables.map(getConfigFromDbTable).forEach(item => {
+        configs = configs.concat(item);
+    });
 
-        const result = writeFiles(configs);
+    const result = writeFiles(configs);
 
-        res.send(result);
-    } catch (e) {
-        res.status(400).send(e);
-    }
+    res.send(result);
 });
 
 router.get('/swagger', async (req, res, next) => {
     const {swaggerUrl, method, userName, password} = req.query;
-    try {
-        const {origin, pathname} = new URL(swaggerUrl);
-        const url = `${origin}/v2/api-docs`; // TODO swagger版本是啥？
+    const {origin, pathname} = new URL(swaggerUrl);
+    const url = `${origin}/v2/api-docs`; // TODO swagger版本是啥？
 
-        const config = {
-            url,
-            userName,
-            password,
-        };
-        const search = method === 'get' ? {
-            method,
-            url: pathname,
-            dataPath: '', // TODO
-            excludeFields: COMMON_EXCLUDE_FIELDS,
-        } : null;
+    const config = {
+        url,
+        userName,
+        password,
+    };
+    const search = method === 'get' ? {
+        method,
+        url: pathname,
+        dataPath: '', // TODO
+        excludeFields: COMMON_EXCLUDE_FIELDS,
+    } : null;
 
-        const modify = method !== 'get' ? {
-            method,
-            url: pathname,
-            dataPath: '', // TODO
-            excludeFields: COMMON_EXCLUDE_FIELDS,
-        } : null;
+    const modify = method !== 'get' ? {
+        method,
+        url: pathname,
+        dataPath: '', // TODO
+        excludeFields: COMMON_EXCLUDE_FIELDS,
+    } : null;
 
-        const baseConfig = {
-            ajax: {
-                search,
-                modify,
-            },
-        };
+    const baseConfig = {
+        ajax: {
+            search,
+            modify,
+        },
+    };
 
-        const result = await readSwagger(config, baseConfig);
-        result.moduleName = pathname.replace(/\//g, '-');
-        if (result.moduleName.startsWith('-')) result.moduleName = result.moduleName.replace('-', '');
+    const result = await readSwagger(config, baseConfig);
+    result.moduleName = pathname.replace(/\//g, '-');
+    if (result.moduleName.startsWith('-')) result.moduleName = result.moduleName.replace('-', '');
 
-        res.send(result);
-    } catch (e) {
-        res.status(400).send(e);
-    }
+    res.send(result);
 });
 module.exports = router;
