f7e5c1b1c0547fb9edc2fe4b9c2204356eec82b5
zkboys <876213774@qq.com>
2021-08-26 17:22:06 +0800

    :art: 整理代码

--- generator/index.js
+++ generator/index.js
@@ -5,19 +5,18 @@ require('express-async-errors');
 const bodyParser = require('body-parser');
 const router = require('./router');
 
-module.exports = function(app, { apiPrefix = '/api' } = {}) {
-  // 生成器静态文件目录
-  app.use('/ra-gen/', express.static(path.join(__dirname, 'public')));
+module.exports = function(app, {apiPrefix = '/api'} = {}) {
+    // 生成器静态文件目录
+    app.use('/ra-gen/', express.static(path.join(__dirname, 'public')));
 
-  // 使用 body-parser 中间
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+    // 生成器首页路由
+    app.get('/dev-ra-gen', function(req, res) {
+        res.setHeader('Content-Type', 'text/html');
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
 
-  // 生成器首页路由
-  app.get('/dev-ra-gen', function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-
-  router.forEach(item => app.use(apiPrefix + '/ra-gen', item));
+    // 使用 body-parser 中间
+    app.use(apiPrefix + '/ra-gen', bodyParser.urlencoded({extended: true}));
+    app.use(apiPrefix + '/ra-gen', bodyParser.json());
+    router.forEach(item => app.use(apiPrefix + '/ra-gen', item));
 };
